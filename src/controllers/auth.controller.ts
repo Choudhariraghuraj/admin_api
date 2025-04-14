edf805cc7606d72cb379f24b766f684cda92c397
choudhariraghuraj <choudhariraghuraj@gmail.com>
2025-04-15 00:05:37 +0530

    Password issue resolved

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -17,11 +17,10 @@ export const register = async (req: Request, res: Response) => {
   const existing = await User.findOne({ email });
   if (existing) return res.status(400).json({ message: "Email already exists" });
 
-  const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password,
     role,
     avatar
   });
@@ -46,7 +45,6 @@ export const login = async (req: Request, res: Response) => {
 
   const user = await User.findOne({ email });
   if (!user) return res.status(404).json({ message: "User not found" });
-
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) return res.status(401).json({ message: "Invalid password" });
 
