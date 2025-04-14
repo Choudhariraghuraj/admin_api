12ab156886e787b07e489c20b7d6adaabab8ee00
choudhariraghuraj <choudhariraghuraj@gmail.com>
2025-04-14 18:23:05 +0530

    Quick fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -12,7 +12,7 @@ const generateToken = (id: string) => {
 
 // @route   POST /api/auth/register
 export const register = async (req: Request, res: Response) => {
-  const { name, email, password, role, avatar } = req.body;
+  const { name, email, password, role = "user", avatar } = req.body;
 
   const existing = await User.findOne({ email });
   if (existing) return res.status(400).json({ message: "Email already exists" });
