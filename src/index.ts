7724211b89d89f1b825a4d9cd3174d86736c6959
choudhariraghuraj <choudhariraghuraj@gmail.com>
2025-04-16 20:57:00 +0530

    Quick fix

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/dashboard", dashboardRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 mongoose.connect(process.env.MONGO_URI!).then(() => {
